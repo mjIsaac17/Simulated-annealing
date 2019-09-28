461caddd8e3dd8c8e1a3b253c90d09df8999e302
Isaac <isaac1998188@gmail.com>
2019-09-28 11:40:19 -0700

    funcion ackley, mide el criterio de finalizacion

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -3,6 +3,7 @@
 # Created: 9/28/2019
 # Modified: 
 import random as r
+import math 
 
 
 
@@ -11,3 +12,8 @@ import random as r
 INITIAL_TEMPERATURE = 20
 TEMPERATURE_REDUCTION_FACTOR = 0.5
 ITERATIONS_EACH_TEMPERATURE = 10
+
+#finalization criteria is evaluated by the ackley function 
+def ackley_function(x,y):
+    f = -20*math.exp(-0.2*math.sqrt(0.5*(x**2+y**2))) - math.exp(0.5*(math.cos(2*math.pi*x) + math.cos(2*math.pi*y))) + math.e + 20
+    return f
