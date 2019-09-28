5774035e9d7b617f6c27393488206bc0a099c407
Isaac <isaac1998188@gmail.com>
2019-09-28 11:04:49 -0700

    inicializacion del programa, declaracion de variables globales

---
+++ simulated-annealing.py
@@ -0,0 +1,13 @@
+# Simulated annealing 
+# By: Isaac Montes Jiménez
+# Created: 9/28/2019
+# Modified: 
+import random as r
+
+
+
+#initial solution:
+#solution = pending
+INITIAL_TEMPERATURE = 20
+TEMPERATURE_REDUCTION_FACTOR = 0.5
+ITERATIONS_EACH_TEMPERATURE = 10
