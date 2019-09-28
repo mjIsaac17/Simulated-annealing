4a9f37794fe6befa96c6f8ced88240781e6660d2
Isaac <isaac1998188@gmail.com>
2019-09-28 13:44:11 -0700

    nueva funcion exploration para explorar el vecindario

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -13,6 +13,9 @@ ITERATIONS_EACH_TEMPERATURE = 10
 MAX_R = 5
 MIN_R = -5
 
+#Maximum amount to explore the neighborhood
+MAX_STEP = 0.5
+
 #objective function (ackley function)
 def fn_ackley_function(x,y):
     f = -20*math.exp(-0.2*math.sqrt(0.5*(x**2+y**2))) - math.exp(0.5*(math.cos(2*math.pi*x) + math.cos(2*math.pi*y))) + math.e + 20
@@ -23,3 +26,8 @@ def fn_initialize_solution():
     x = r.uniform(MIN_R, MAX_R)
     y = r.uniform(MIN_R, MAX_R)
     return x, y
+
+def fn_exploration():
+    step_x = r.uniform(0,MAX_STEP)
+    step_y = r.uniform(0,MAX_STEP)
+    return step_x, step_y
