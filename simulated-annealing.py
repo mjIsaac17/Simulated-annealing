d031979a249dbb95633bf0441f01db0e9d76e4f8
Isaac <isaac1998188@gmail.com>
2019-09-29 10:49:22 -0700

    correcion de la funcion objetivo goldstein_price

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -40,8 +40,10 @@ def fn_beale_function(x,y):
     return f
 
 def fn_goldstein_price_function(x,y):
-    f = ( (1+(x+y+1)**2) * (19 - 14*x + (3*x**2) - 14*y + 6*x*y + (3*y**2)) ) * (30 + ((2*x - 3*y)**2) * (18 - 32*x + (12*x**2) + 48*y - 36*x*y + (27*y**2)) ) 
+    f = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2) ) 
     return f
+
+print(fn_goldstein_price_function(0,-1))
 
 def fn_initialize_solution_akley():
     x = r.uniform(MIN_R_ACKLEY, MAX_R_ACKLEY)
@@ -171,4 +173,4 @@ def fn_mainSA_goldstein_price():
 
 #fn_mainSA_ackley()
 #fn_mainSA_beale()
-fn_mainSA_goldstein_price()
+#fn_mainSA_goldstein_price()
