531c108565e7547247dad030481c80afd6d3d56e
Isaac <isaac1998188@gmail.com>
2019-09-29 11:08:27 -0700

    se agrega uncion objetivo 'easom'

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -26,9 +26,13 @@ MIN_R_BEALE = 4.5
 MAX_R_GOLDSTEIN = 2
 MIN_R_GOLDSTEIN = -2
 
+#Easom function
+MAX_R_EASOM = 10
+MIN_R_EASOM = -10
+
 
 #Maximum amount to both sides to explore the neighborhood
-MAX_STEP = 0.5 
+MAX_STEP = -0.5
 
 #objective function (ackley function), evaluates the energy
 def fn_ackley_function(x,y):
@@ -43,8 +47,12 @@ def fn_goldstein_price_function(x,y):
     f = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2) ) 
     return f
 
-print(fn_goldstein_price_function(0,-1))
+def fn_easom_function(x,y):
+    f = -1 * math.cos(x) * math.cos(y) * math.exp(-1 * ((x - math.pi)**2 + (y - math.pi)**2))
+    return f
+print(fn_easom_function(5.990397119524389,13.000477673369343))
 
+#Random initialization by objective function range
 def fn_initialize_solution_akley():
     x = r.uniform(MIN_R_ACKLEY, MAX_R_ACKLEY)
     y = r.uniform(MIN_R_ACKLEY, MAX_R_ACKLEY)
@@ -60,11 +68,18 @@ def fn_initialize_solution_goldstein():
     y = r.uniform(MIN_R_GOLDSTEIN, MAX_R_GOLDSTEIN)
     return x, y
 
+def fn_initialize_solution_easom():
+    x = r.uniform(MIN_R_EASOM, MAX_R_EASOM)
+    y = r.uniform(MIN_R_EASOM, MAX_R_EASOM)
+    return x, y
+
+#Explore the neighborhood
 def fn_exploration():
     step_x = r.uniform(-1*MAX_STEP, MAX_STEP)
     step_y = r.uniform(-1*MAX_STEP, MAX_STEP)
     return step_x, step_y
 
+#Plot the results
 def fn_plot(array_x, array_y, array_energy, title):   
     fig = plot.figure()
     ax = fig.add_subplot(111,projection='3d')
@@ -75,6 +90,7 @@ def fn_plot(array_x, array_y, array_energy, title):
     ax.plot_trisurf(array_x, array_y,array_energy, cmap=plot.cm.Spectral, antialiased=False)
     plot.show()
 
+#Main function by objective function
 def fn_mainSA_ackley():
     x, y = fn_initialize_solution_akley()
     energy = fn_ackley_function(x,y)
@@ -171,6 +187,39 @@ def fn_mainSA_goldstein_price():
     print("Y: ", y)
     fn_plot(array_x, array_y, array_energy, 'Goldstein-price function')
 
+def fn_mainSA_easom():
+    x, y = fn_initialize_solution_easom()
+    energy = fn_easom_function(x,y)
+    temperature = INITIAL_TEMPERATURE
+    array_x = []
+    array_y = []
+    array_energy = []
+    while(temperature > FINAL_TEMPERATURE): #termination criteria
+        for i in range(ITERATIONS_EACH_TEMPERATURE):
+            step_x, step_y = fn_exploration()
+            new_energy = fn_easom_function(x+step_x, y+step_y)
+            delta_energy = new_energy - energy
+            if(delta_energy <= 0):
+                x += step_x
+                y += step_y
+                energy = new_energy
+            else:
+                rand = r.random()
+                if(rand < math.exp((-1*delta_energy)/temperature)):
+                    x += step_x
+                    y += step_y
+                    energy = new_energy
+            array_x.append(x)
+            array_y.append(y)
+            array_energy.append(energy)
+        temperature *= TEMPERATURE_REDUCTION_FACTOR
+        print(temperature)
+    print("Final value of objective function: ", energy)
+    print("X: ", x)
+    print("Y: ", y)
+    fn_plot(array_x, array_y, array_energy, 'Easom function')
+
 #fn_mainSA_ackley()
 #fn_mainSA_beale()
 #fn_mainSA_goldstein_price()
+#fn_mainSA_easom()
