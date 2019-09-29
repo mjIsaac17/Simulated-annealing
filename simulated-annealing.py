f772fffc25637941ab78db39711dd49171f8f5ee
Isaac <isaac1998188@gmail.com>
2019-09-29 10:20:14 -0700

    se agrega funcion objetivo 'Beale' y titulo al plot

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -13,9 +13,12 @@ FINAL_TEMPERATURE = 0.05
 TEMPERATURE_REDUCTION_FACTOR = 0.95
 ITERATIONS_EACH_TEMPERATURE = 10
 
-#Define the range of the values of the objective function 
-MAX_R = 5
-MIN_R = -5
+#Define the range of the values by the objective function 
+MAX_R_ACKLEY = 5
+MIN_R_ACKLEY = -5
+
+MAX_R_BEALE = 4.5
+MIN_R_BEALE = 4.5
 
 #Maximum amount to both sides to explore the neighborhood
 MAX_STEP = 0.5 
@@ -24,11 +27,19 @@ MAX_STEP = 0.5
 def fn_ackley_function(x,y):
     f = -20*math.exp(-0.2*math.sqrt(0.5*(x**2+y**2))) - math.exp(0.5*(math.cos(2*math.pi*x) + math.cos(2*math.pi*y))) + math.e + 20
     return f
-#print(fn_ackley_function(x,y))
 
-def fn_initialize_solution():
-    x = r.uniform(MIN_R, MAX_R)
-    y = r.uniform(MIN_R, MAX_R)
+def fn_beale_function(x,y):
+    f = (1.5-x+x*y)**2 + (2.25-x+(x*y**2))**2 + (2.625-x+(x*y**3))**2
+    return f
+
+def fn_initialize_solution_akley():
+    x = r.uniform(MIN_R_ACKLEY, MAX_R_ACKLEY)
+    y = r.uniform(MIN_R_ACKLEY, MAX_R_ACKLEY)
+    return x, y
+
+def fn_initialize_solution_beale():
+    x = r.uniform(MIN_R_BEALE, MAX_R_BEALE)
+    y = r.uniform(MIN_R_BEALE, MAX_R_BEALE)
     return x, y
 
 def fn_exploration():
@@ -36,17 +47,18 @@ def fn_exploration():
     step_y = r.uniform(-1*MAX_STEP, MAX_STEP)
     return step_x, step_y
 
-def fn_plot(array_x, array_y, array_energy):   
+def fn_plot(array_x, array_y, array_energy, title):   
     fig = plot.figure()
     ax = fig.add_subplot(111,projection='3d')
+    ax.set_title(title)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z') 
     ax.plot_trisurf(array_x, array_y,array_energy, cmap=plot.cm.Spectral, antialiased=False)
     plot.show()
 
-def fn_main_SA_ackleyF():
-    x, y = fn_initialize_solution()
+def fn_mainSA_ackley():
+    x, y = fn_initialize_solution_akley()
     energy = fn_ackley_function(x,y)
     temperature = INITIAL_TEMPERATURE
     array_x = []
@@ -75,6 +87,39 @@ def fn_main_SA_ackleyF():
     print("Final value of objective function: ", energy)
     print("X: ", x)
     print("Y: ", y)
-    fn_plot(array_x, array_y, array_energy)
+    fn_plot(array_x, array_y, array_energy,'Ackley function')
 
-fn_main_SA_ackleyF()
+def fn_mainSA_beale():
+    x, y = fn_initialize_solution_beale()
+    energy = fn_beale_function(x,y)
+    temperature = INITIAL_TEMPERATURE
+    array_x = []
+    array_y = []
+    array_energy = []
+    while(temperature > FINAL_TEMPERATURE): #termination criteria
+        for i in range(ITERATIONS_EACH_TEMPERATURE):
+            step_x, step_y = fn_exploration()
+            new_energy = fn_beale_function(x+step_x, y+step_y)
+            delta_energy = new_energy - energy
+            if(delta_energy <= 0):
+                x += step_x
+                y += step_y
+                energy = new_energy
+            else:
+                rand = r.random()
+                if(rand < math.exp((-1*delta_energy)/temperature)):
+                    x += step_x
+                    y += step_y
+                    energy = new_energy
+            array_x.append(x)
+            array_y.append(y)
+            array_energy.append(energy)
+        temperature *= TEMPERATURE_REDUCTION_FACTOR
+        print(temperature)
+    print("Final value of objective function: ", energy)
+    print("X: ", x)
+    print("Y: ", y)
+    fn_plot(array_x, array_y, array_energy, 'Beale function')
+
+#fn_mainSA_ackley()
+fn_mainSA_beale()
